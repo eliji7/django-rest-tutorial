b697702eacffff702579441a1352dc00d3530937
Eli Ji <eji@Rediker.com>
2018-05-03 10:56:29 -0400

    overoad perform_create method to pass owner field to serializer

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -8,9 +8,9 @@ from django.contrib.auth.models import User
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    
-
-
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+        
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
@@ -18,7 +18,6 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
 
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
