7fd8beef5e832f9ffdae459ba81d75ed189c6384
Eli Ji <eji@Rediker.com>
2018-05-03 10:58:29 -0400

    added required permissions

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -3,15 +3,18 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from snippets.serializers import UserSerializer
 from rest_framework import generics
+from rest_framework import permissions
 from django.contrib.auth.models import User
 
 class SnippetList(generics.ListCreateAPIView):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
-        
+
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
