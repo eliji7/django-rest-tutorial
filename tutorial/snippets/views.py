2acc1d8b6365be5134c738550ec57d5960140ed0
Eli Ji <eji@Rediker.com>
2018-05-03 10:50:07 -0400

    added fields and save method to snippet model, created user serializer and user views

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,14 +1,25 @@
 # -*- coding: utf-8 -*-
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+from snippets.serializers import UserSerializer
 from rest_framework import generics
-
+from django.contrib.auth.models import User
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
