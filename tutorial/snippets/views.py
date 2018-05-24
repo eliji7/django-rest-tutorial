394b49a89421c036f30e67b526e150741937e2e8
Eli Ji <eji@Rediker.com>
2018-05-24 11:19:35 -0400

    create olp and add to snippet detail... also realize should use present tense :)

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -5,6 +5,7 @@ from snippets.serializers import UserSerializer
 from rest_framework import generics
 from rest_framework import permissions
 from django.contrib.auth.models import User
+from snippets.permissions import IsOwnerOrReadOnly
 
 class SnippetList(generics.ListCreateAPIView):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
@@ -17,6 +18,8 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                      IsOwnerOrReadOnly,)
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
