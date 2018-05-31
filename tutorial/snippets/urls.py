b879aa99d8ea4c186f90505c707251c964d2c467
Eli Ji <eji@Rediker.com>
2018-05-31 15:25:53 -0400

    add hyperlinking and highlight field to serializers, add url name

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -5,11 +5,11 @@ from snippets import views
 
 urlpatterns = [
     url(r'^snippets/$', views.SnippetList.as_view(), name = 'snippet-list'),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view(), name = 'snippet-detail'),
     url(r'^users/$', views.UserList.as_view(), name = 'user-list'),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
     url(r'^$', views.api_root),
-    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view(), name = "snippet-highlight"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
