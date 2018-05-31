01689f5d49a6f528301eb289f6e0c87026d6544d
Eli Ji <eji@Rediker.com>
2018-05-31 14:57:03 -0400

    add view for snippet highlight, add url for api root and snippet highlight

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -9,6 +9,8 @@ from snippets.permissions import IsOwnerOrReadOnly
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+from rest_framework import renderers
+from rest_framework.response import Response
 
 @api_view(['GET'])
 def api_root(request, format=None):
@@ -30,6 +32,14 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = SnippetSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
                       IsOwnerOrReadOnly,)
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
