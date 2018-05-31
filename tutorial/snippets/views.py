3c7fffb30266c0c1fbd6debda89815e9600e780d
Eli Ji <eji@Rediker.com>
2018-05-31 16:11:41 -0400

    add missing import in views and bind viewsets explicitly to urls

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -4,6 +4,7 @@ from rest_framework import generics, permissions, renderers, viewsets
 from rest_framework.decorators import api_view, detail_route
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+from rest_framework.decorators import action
 
 from snippets.models import Snippet
 from snippets.permissions import IsOwnerOrReadOnly
