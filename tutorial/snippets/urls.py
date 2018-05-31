02cbe3f680c3c886e8e9d2983908f82c201987d5
Eli Ji <eji@Rediker.com>
2018-05-31 15:58:22 -0400

    add missing url name and pagination

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^snippets/$', views.SnippetList.as_view(), name = 'snippet-list'),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view(), name = 'snippet-detail'),
     url(r'^users/$', views.UserList.as_view(), name = 'user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name = 'user-detail'),
     url(r'^$', views.api_root),
     url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view(), name = "snippet-highlight"),
 ]
